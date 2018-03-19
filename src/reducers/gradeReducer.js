cf3f2cf609010de8edee72f68250030ea7eed993
marklain <marklain.work@gmail.com>
2018-03-19 21:49:42 +0200

    added build with added feature with localStorage

--- src/reducers/gradeReducer.js
+++ src/reducers/gradeReducer.js
@@ -1,6 +1,6 @@
 import v4 from 'uuid';
 
-const initialState = [{
+const initialState = JSON.parse(localStorage.getItem('grades-info')) || [{
     id: v4(),
     name: '8a',
     students: [{
@@ -18,26 +18,34 @@ const initialState = [{
 export default function(state = initialState,action) {
     switch (action.type){
         case 'ADD_GRADE':
+            localStorage.setItem('grades-info', JSON.stringify(action.payload));
             return action.payload;
         case 'DELETE_STUDENT':
+            localStorage.setItem('grades-info', JSON.stringify(action.payload));
             return action.payload;
         case 'ADD_STUDENT':
-            return state.map(grade => {
+            let newGrade = state.map(grade => {
                 if (grade.isActive) {
                     grade.students = [...grade.students, action.payload];
                     return grade;
                 } else return grade;
             });
+            localStorage.setItem('grades-info', JSON.stringify(newGrade));
+            return newGrade;
         case 'CHANGE_GRADE_NAME':
-            return state.map(grade => {
+            let newGrades = state.map(grade => {
                 if (grade.isActive) {
                     grade.name = action.payload;
                     return grade;
                 } else return grade;
             });
+            localStorage.setItem('grades-info', JSON.stringify(newGrades));
+            return newGrades;
         case 'SET_ACTIVE_GRADE':
+            localStorage.setItem('grades-info', JSON.stringify(action.payload));
             return action.payload;
         case 'DELETE_GRADE':
+            localStorage.setItem('grades-info', JSON.stringify(action.payload));
             return action.payload;
         default: return state;
     }
